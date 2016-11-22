8ad62f521ac1ab13d04742194e26304cbe2fde1b
AndrewBell <andrew@recursivechaos.com>
2016-11-21 22:24:11 -0600

    Listener example

---
+++ README.md
@@ -0,0 +1,12 @@
+# Thrawn #
+
+# Setup #
+
+1. Create Bot in Slack
+2. Grab API Key
+3. Update `scraper\application.properties`
+4. Invite to Channel
+
+# Run #
+
+`node scraper/index.js`
---
+++ scraper/index.js
@@ -0,0 +1,21 @@
+var PropertiesReader = require('properties-reader');
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+// Get bot token
+var properties = PropertiesReader('application.properties');
+var bot_token = properties.get('slack.api.key');
+
+var rtm = new RtmClient(bot_token);
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+});
+
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+    console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+});
+
+rtm.start();
